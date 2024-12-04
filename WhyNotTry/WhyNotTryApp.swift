34d50b28730206a2da20f0d9ea086e7e4a66597e
Roland Labana <rolandlabana@gmail.com>
2024-12-04 15:27:39 -0800

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Roland Labana on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhyNotTryTests/WhyNotTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryTests.swift
+//  WhyNotTryTests
+//
+//  Created by Roland Labana on 12/4/24.
+//
+
+import Testing
+@testable import WhyNotTry
+
+struct WhyNotTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
