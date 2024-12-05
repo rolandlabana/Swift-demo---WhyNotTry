89b9ad74f79bbd556e84fe96a9e835d23a1ff8cd
Roland Labana <rolandlabana@gmail.com>
2024-12-04 18:32:51 -0800

    adding a few color features

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+
+    @State private var selected = "Baseball"
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Why not try…")
+                .font(.largeTitle.bold())
+            
+            VStack {
+                
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay (Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                    )
+                
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            Spacer()
+            
+            Button("Try again") {
+                selected = activities.randomElement()  ?? "Archery"          }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
